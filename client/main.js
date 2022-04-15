4d01a5aa57e38bb1cfb8d5b0a41ffbee3156b5b1
Serena Sorensen <serena.sorensen7@gmail.com>
2022-04-15 12:29:06 -0600

    added API for random activity selection

--- client/main.js
+++ client/main.js
@@ -1,7 +1,8 @@
 const baseURL = "http://localhost:4000/api";
 const addForm = document.getElementById('addActivity');
 const container = document.getElementById('activity-container');
-
+const getRand = document.getElementById('getRandAct');
+const randomSect = document.getElementById('random-sect');
 //-------------------------------------------------------------------//
 
 document.getElementById("complimentButton").onclick = function () {
@@ -76,6 +77,62 @@ function displayActivities(arr){
     };
 };
 
-addForm.addEventListener('submit',addSubmitHandler);
+function addRandom(act,url){
+    const randRatingOpt = document.querySelector('#randRating');
+    const randRating = randRatingOpt.options[randRatingOpt.selectedIndex];
+    const newObj = {
+        activity: act,
+        imgURL: url,
+        rating: randRating.value
+    }
+    createActivity(newObj);
 
+    clearRandom();
+}
+
+function clearRandom(evt) {
+    const div = document.querySelectorAll('#random-sect>div');
+    for(let i = 0; i < div.length; i++){
+        div[i].remove();
+    }
+}
+
+function randSubmitHandler(e){
+    e.preventDefault();
+    clearRandom();
+    axios.get('http://www.boredapi.com/api/activity/').then(res => {
+        randActivity = res.data.activity;
+        randImgURL = res.data.link;
+        const newDiv = document.createElement('div');
+        newDiv.innerHTML = `
+        <h2 id="randomAct">${randActivity}</h2>
+        <a id="randLink" href=${randImgURL}>link</a>
+        <form id="randomAddForm">
+            <div id="rand-div">
+                <label for="randRating">Choose a Rating:</label>
+                <select id="randRating" name="randRating">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
+            </div>
+            <button id="randAddBtn">Add to List</button>
+        </form>
+        <button id="randClearBtn">Clear</button>
+        `
+        randomSect.appendChild(newDiv);
+        const randomAddForm = document.querySelector("#randAddBtn");
+        randomAddForm.addEventListener('click',function(e){
+            e.preventDefault();
+            addRandom(randActivity,randImgURL)
+        })
+        const clearBtn = document.querySelector('#randClearBtn');
+        clearBtn.addEventListener('click',clearRandom);
+    })
+}
+
+addForm.addEventListener('submit',addSubmitHandler);
+getRandAct.addEventListener('click',randSubmitHandler);
 getAllActivities();
